3ad775465e3e0d35e957b3f1aef8c6b40abc9314
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-08-10 21:49:39 +0100

    Initial Build

---
+++ README.md
@@ -0,0 +1,26 @@
+# Studio Ghibli Fetch
+
+A Google Cloud function to retrieve data from the ghibliapi.herokuapp.com/films api.  
+
+The data returned has the description fields cropped to a max of 300 characters, plus
+escape-html used to escape the data before returning it (as this is a third-party api).
+
+Access-Control headings set to allow the function to be called from a specific URL, and
+from localhost to allow testing.
+
+This requires the node v8 runtime
+
+## Uploading to Cloud Functions
+
+Install the gcloud utilities then use command
+
+```bash
+gcloud beta functions deploy {function name} --runtime nodejs8 --trigger-http --project {project name}
+```
+
+to update the function.
+
+## Environment Variables
+
+set `defaultOrigin` to the required URL, for instance `https://example.test.com`
+set `endPoint` to the Studio Ghibli API, for instance `https://ghibliapi.herokuapp.com/films`
---
+++ index.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const escapeHtml = require('escape-html');
+const { accessControl } = require('@neil188/setaccesscontrol');
+
+const URL = process.env.endPoint;
+const setAccessControl = accessControl(process.env.defaultOrigin, true);
+
+const shorten = (str = '', len = 300, trail='...') => {
+    if (str.len <=len) return str;
+    const pos = str.lastIndexOf(' ', len - trail.length);
+
+    return pos <= 0 ?
+        `${str.substring(0, len-trail.lengt)}${trail}` :
+        `${str.substring(0, pos)}${trail}`
+}
+
+exports.studioghiblifetch = async (req, res) => {
+
+    // set CORS headers
+    setAccessControl(res, req.get('origin'));
+
+    // Send response to OPTIONS requests and terminate the function execution
+    if (req.method == 'OPTIONS') {
+        res.status(204).send('');
+    }
+
+    try {
+        const response = await fetch(URL);
+        const result = await response.json();
+
+        const data  = result.map(
+            ({ title, description }) =>
+                ({
+                    title,
+                    description: escapeHtml(shorten(description)),
+                }) )
+
+        res.json(data)
+    } catch(e) {
+        res.sendStatus(500);
+    }
+
+};
